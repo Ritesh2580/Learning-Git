601cb51841357c9e12501f0c6062b8bd89ca2b48
Ritesh <riteshshakya2580@gmail.com>
2022-09-07 13:49:47 +0530

    "Learning git commit 2"

--- src/main/java/SomeClass.java
+++ src/main/java/SomeClass.java
@@ -2,5 +2,7 @@ public class SomeClass {
     public static void main(String gg[])
     {
         System.out.println("Some class main called");
+        System.out.println("Some class main change 1");
+
     }
 }
