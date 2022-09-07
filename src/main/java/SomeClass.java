92b586d3c395c7729944c7d05b6cf7a32d5b39c5
Ritesh <riteshshakya2580@gmail.com>
2022-09-07 13:43:47 +0530

    "Learning git commit 1"

---
+++ src/main/java/SomeClass.java
@@ -0,0 +1,6 @@
+public class SomeClass {
+    public static void main(String gg[])
+    {
+        System.out.println("Some class main called");
+    }
+}
---
+++ src/test/java/SomClassTest.java
@@ -0,0 +1,6 @@
+public class SomClassTest {
+    public static void main(String arguments[])
+    {
+        System.out.println("someclasstest main called.");
+    }
+}
